99ed1504ee538fac7d0c37f236096dacb1f04189
marco <arnaud.marcantoine@gmail.com>
2015-01-04 21:36:28 +0100

    adding rendering engine for basic scene

---
+++ render/renderScene.py
@@ -0,0 +1,33 @@
+import logging
+from pyTuttle import tuttle
+
+class RenderScence:
+	project = None
+	graph = None
+	nodes = []
+	def __init__(self):
+		tuttle.core().preload(False)
+		self.graph = tuttle.Graph()
+
+	def setProject(self, project, outputFilename):
+		self.project = project
+		
+		try:
+			for node in project['nodes']:
+				tuttleNode = self.graph.createNode(str(node['plugin']))
+				self.nodes.append(tuttleNode)
+
+			for connection in project['connections']:
+				self.graph.connect( [self.nodes[connection['src']], self.nodes[connection['dst']]] )
+
+			outputFilenameParameter = self.nodes[len(self.nodes)-1].getParam("filename")
+			outputFilenameParameter.setValue( outputFilename )
+
+		except Exception as e:
+			logging.info("Error in setup of the project: " + str(e))
+
+	def render(self):
+		try:
+			self.graph.compute( self.nodes[len(self.nodes)-1] )
+		except Exception as e:
+			logging.info("Error in render: " + str(e))
--- render/runRender.py
+++ render/runRender.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
+import os
+import tempfile
 from flask import Flask, request, jsonify
 import renderScene
+
+currentAppDir = os.path.dirname(__file__)
+tmpRenderingPath = os.path.join(currentAppDir, "tmp")
+if not os.path.exists(tmpRenderingPath):
+  os.mkdir(tmpRenderingPath)
 
 app = Flask(__name__, static_folder='', static_url_path='')
 
@@ -19,14 +26,23 @@ def newRender():
     # print request.form
     # print request.args
     # print request.data
+    project = request.get_json()
+    user = 'marco'
+    tmpFile = tempfile.mkstemp(prefix='tuttle_', suffix="_"+user+".jpg", dir=tmpRenderingPath)
     renderId = 0
     if len(renders) > 0:
         renderId = 1 + int(renders[len(renders)-1]['id'])
 
-    ret = { "id" : renderId, "scene" : request.get_json() }
+    ret = {
+      "id" : renderId,
+      "scene" : project,
+      "resources": [
+        "/render/"+str(renderId)+"/resources/"+os.path.basename(tmpFile[1])
+      ]}
     renders.append( ret )
 
     r = renderScene.RenderScence()
+    r.setProject(project, outputFilename=tmpFile[1])
     r.render()
 
     return jsonify(**ret)
@@ -36,9 +52,18 @@ def getRenders():
     totalRenders = {"renders": renders}
     return jsonify(**totalRenders)
 
-@app.route('/render/<renderId>', methods=['GET'])
+@app.route('/render/<int:renderId>', methods=['GET'])
 def getRender(renderId):
     return renderId
+
+@app.route('/render/<int:renderId>/resources/', methods=['GET'])
+def resources(renderId):
+    resources = { "resources": []}
+    return jsonify(**resources)
+  
+@app.route('/render/<int:renderId>/resources/<resourceId>', methods=['GET'])
+def resource(renderId, resourceId):
+  return app.send_static_file('tmp/' + resourceId)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5011, debug=True)
