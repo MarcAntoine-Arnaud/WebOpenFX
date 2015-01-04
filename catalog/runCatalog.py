c822bc13244214bb12c8422aa258e447de581b5c
marco <arnaud.marcantoine@gmail.com>
2015-01-04 16:05:08 +0100

    get plugins and plugin properties

--- catalog/runCatalog.py
+++ catalog/runCatalog.py
@@ -1,16 +1,54 @@
 #!/usr/bin/python
-
-from flask import Flask
+import logging
+from flask import Flask, jsonify
+from pyTuttle import tuttle
 
 app = Flask(__name__, static_folder='', static_url_path='')
 
 version = "0.0.1"
 
+ofxPluginPath = "/home/marco/dev/TuttleOFX/distScons/marco-N150P-N210P-N220P/gcc-4.8/production/plugin/"
+tuttle.core().getPluginCache().addDirectoryToPath(ofxPluginPath)
+pluginCache = tuttle.core().getPluginCache()
+tuttle.core().preload(False)
+plugins = pluginCache.getPlugins()
+
 @app.route('/', methods=['GET'])
 def index():
-    index = "<html><head><title>WebOpenFX - Catalog</title></head><body><h1><center>WebOpenFX - Catalog</center></h1><br/><br/><ul><li>version: " + str(version) + "</li></ul></body>"
-    return index
+  index = "<html><head><title>WebOpenFX - Catalog</title></head><body><h1><center>WebOpenFX - Catalog</center></h1><br/><br/><ul><li>version: " + str(version) + "</li></ul></body>"
+  return index
 
+def getPluginProperties(plugin):
+  logging.info('Analyzing plugin for ' + plugin.getRawIdentifier())
+
+  pluginProperties = {
+    'id': plugin.getIdentifier(),
+    'rawIdentifier': plugin.getRawIdentifier(),
+    'version': {
+      'major': plugin.getVersionMajor(),
+      'minor': plugin.getVersionMinor()
+    },
+    'pluginApi': plugin.getPluginApi(),
+    'pluginApiVersion': plugin.getApiVersion()
+
+  }
+
+  return pluginProperties
+
+@app.route('/plugins', methods=['GET'])
+def getPlugins():
+  pluginsDescription = {'plugins':[]}
+  for plugin in plugins:
+    pluginsDescription['plugins'].append(getPluginProperties(plugin))
+
+  return jsonify(**pluginsDescription)
+
+@app.route('/plugins/<string:pluginId>', methods=['GET'])
+def getPlugin(pluginId):
+  plugin = pluginCache.getPluginById(str(pluginId))
+  pluginDescription = getPluginProperties(plugin)
+  return jsonify(**pluginDescription)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5010, debug=True)
+  logging.getLogger().setLevel(10)
+  app.run(host='0.0.0.0', port=5010, debug=True)
