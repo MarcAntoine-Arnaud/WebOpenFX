599b84979dd7b5662e0ae8b04e41137ca24166e9
marco <arnaud.marcantoine@gmail.com>
2015-01-28 23:13:58 +0100

    update client configuration

--- runClient.py
+++ runClient.py
@@ -3,6 +3,6 @@ from client import app
 import ConfigParser
 
 configParser =  ConfigParser.RawConfigParser()
-configParser.read('client/configuration.conf')
+configParser.read('configuration.conf')
 
 app.run(host=configParser.get("APP_CLIENT", "host"), port=configParser.getint("APP_CLIENT", "port"), debug=True)
